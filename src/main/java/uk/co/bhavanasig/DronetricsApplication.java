9bac8c70b01af4987063249426614bd820977d60
Ganesh Bhavanasi <ganesh.bhavanasi@fivium.co.uk>
2026-03-30 15:22:40 +0100

    build: Initial setup

---
+++ src/main/java/uk/co/bhavanasig/DronetricsApplication.java
@@ -0,0 +1,13 @@
+package uk.co.bhavanasig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronetricsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DronetricsApplication.class, args);
+  }
+
+}
---
+++ src/main/java/uk/co/bhavanasig/drone/controller/DroneApiRestController.java
@@ -0,0 +1,10 @@
+package uk.co.bhavanasig.drone.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO: Implement APIs getting data from Drone API
+ */
+@RestController
+public class DroneApiRestController {
+}
